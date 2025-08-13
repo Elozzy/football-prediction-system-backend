a1253e7a8b74e80dce41094bb7a98e2a2854e25f
Osagie Eloghosa <st.eloghosa@gmail.com>
2025-08-13 17:07:24 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
 
